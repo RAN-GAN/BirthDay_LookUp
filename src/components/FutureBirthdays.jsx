1b44cc2d62bda0e9fa99700b476bb8ada64f9e01
Pradeep Chandran <pradeepmojo1708@gmail.com>
2024-04-11 11:28:57 +0530

    Loading added

--- src/components/FutureBirthdays.jsx
+++ src/components/FutureBirthdays.jsx
@@ -10,28 +10,44 @@ import React, { useEffect, useState } from "react";
 
 function FutureBirthday() {
   const [birthdays, setBirthdays] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://rangandale.pythonanywhere.com/api/future-birthdays")
       .then((response) => response.json())
-      .then((data) => setBirthdays(JSON.parse(data)))
+      .then((data) => {
+        setBirthdays(JSON.parse(data));
+        setLoading(false);
+      })
       .catch((error) => console.error("Error:", error));
   }, []);
 
-  return (
-    <div className="TodaysBirthday">
-      <h1 className="tb">Upcoming Birthdays</h1>
-      <hr />
-      <ul>
-        {birthdays.map((person, index) => (
-          <li key={index}>
-            {index + 1}. {person["Student Name "]} -{" "}
-            {formatDate(person["Date of Birth"])} - {person["Department"]}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (loading) {
+    return (
+      <div className="TodaysBirthday">
+        <h1 className="tb">Upcoming Birthdays</h1>
+        <hr />
+        <ul>
+          <li>Loading...</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="TodaysBirthday">
+        <h1 className="tb">Upcoming Birthdays</h1>
+        <hr />
+        <ul>
+          {birthdays.map((person, index) => (
+            <li key={index}>
+              {index + 1}. {person["Student Name "]} -{" "}
+              {formatDate(person["Date of Birth"])} - {person["Department"]}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default FutureBirthday;
--- src/components/TodaysBirthday.jsx
+++ src/components/TodaysBirthday.jsx
@@ -4,27 +4,43 @@ import React, { useEffect, useState } from "react";
 
 function TodaysBirthday() {
   const [birthdays, setBirthdays] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://rangandale.pythonanywhere.com/api/todays-birthdays")
       .then((response) => response.json())
-      .then((data) => setBirthdays(JSON.parse(data)))
+      .then((data) => {
+        setBirthdays(JSON.parse(data));
+        setLoading(false);
+      })
       .catch((error) => console.error("Error:", error));
   }, []);
 
-  return (
-    <div className="TodaysBirthday">
-      <h1 className="tb">Today's Birthdays</h1>
-      <hr />
-      <ul>
-        {birthdays.map((person, index) => (
-          <li key={index}>
-            {index + 1}. {person["Student Name "]} - {person["Department"]}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (loading) {
+    return (
+      <div className="TodaysBirthday">
+        <h1 className="tb">Todays Birthdays</h1>
+        <hr />
+        <ul>
+          <li>Loading...</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="TodaysBirthday">
+        <h1 className="tb">Today's Birthdays</h1>
+        <hr />
+        <ul>
+          {birthdays.map((person, index) => (
+            <li key={index}>
+              {index + 1}. {person["Student Name "]} - {person["Department"]}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default TodaysBirthday;
