2c00c06e74c50c281b6e7df57eb3143eb3ff4ee5
Pradeep Chandran <pradeepmojo1708@gmail.com>
2024-04-16 20:53:39 +0530

    added login

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,86 @@ import ReactDOM from "react-dom";
 import Header from "./components/Header";
 import TodaysBirthday from "./components/TodaysBirthday";
 import FutureBirthday from "./components/FutureBirthdays";
+import { useState } from "react";
 import "./App.css";
 function App() {
-  return (
-    <>
-      <Header />
-      <div className="container">
-        <TodaysBirthday />
-        <FutureBirthday />
-      </div>
-    </>
-  );
+  const [loggedin, setLoggedin] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function handleLogin(e) {
+    console.log(username);
+    console.log(password);
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Invalid username or password");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setLoggedin(true);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Invalid username or password");
+      });
+  }
+  if (loggedin) {
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <TodaysBirthday />
+          <FutureBirthday />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Header />
+        <div className="Login container">
+          <div className="login">
+            <h3>Login</h3>
+            <form>
+              <label>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </label>
+              <label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              <button onClick={handleLogin} className="LoginButton">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
